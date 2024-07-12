bd7883d0929e68ae2566cdaa611c2aac172bc982
Lucas <lucasfpineram@gmail.com>
2024-07-12 20:29:37 -0300

    avanzando con dbt
    xd

--- dags/run_dbt.py
+++ dags/run_dbt.py
@@ -47,7 +47,7 @@ def copy_docs(project_dir: str):
 with DAG(
     "run_dbt",
     default_args=DEFAULT_ARGS,
-    schedule=None,  # TODO: complete aquí con lo que considere
+    schedule="0 0 * * *",  # TODO: complete aquí con lo que considere
     catchup=False,
     max_active_runs=1,
     tags=["dbt"],
--- dbt_tp/models/example/my_second_dbt_model.sql
+++ dbt_tp/models/example/my_second_dbt_model.sql
@@ -1,6 +1,6 @@
-select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+-- select *
+-- from {{ ref('my_first_dbt_model') }}
+-- where id = 1
 
 -- calificaciones, rango horario
 -- viajes cancelados
